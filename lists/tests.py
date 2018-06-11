5e4aa427af5a4f4ab2b711a4da814a4381f22cc6
Oyetoke Tobi <oyetoketoby80@gmail.com>
2018-06-11 13:01:51 +0100

    First push

---
+++ lists/tests.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.urls import resolve
+from django.test import TestCase
+from views import Homepage
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+# Create your tests here.
+class HomePageTest(TestCase):
+    
+    def test_uses_homepage_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, 'home.html')
+
+
+    # def test_home_page_returns_correct_html(self):
+    #     # request = HttpRequest()
+    #     # response = Homepage(request)
+    #     response = self.client.get("/")
+    #     html = response.content.decode("UTF-8")
+    #     self.assertTrue(html.startswith('<!DOCTYPE html>'))
+    #     self.assertIn('<title>To-Do</title>', html)
+    #     self.assertTrue(html.endswith('</html>'))
+    #     # expected_html = render_to_string('home.html')
+    #     # self.assertEqual(html, expected_html)
+
+        self.assertTemplateUsed(response, 'home.html')
---
+++ tests.py
@@ -0,0 +1,55 @@
+from selenium import webdriver 
+import unittest
+from selenium.webdriver.common.keys import Keys
+import time
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id("add_new_item")
+        print(inputbox)
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys("A new To-Do Item")
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
+
